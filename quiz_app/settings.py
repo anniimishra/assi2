5b0db03ce63c817b2884d25792a5d745818b10bd
Aniket Mishra <aniket.mishra.tushar@gmail.com>
2024-12-12 17:17:47 +0530

    new

--- quiz_app/settings.py
+++ quiz_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-95z@8(g8ui2dx)pgz$l1tq8*^y@ecl6pss^2n22s8@k4edv(r8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
